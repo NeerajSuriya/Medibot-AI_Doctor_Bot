115309b02f5edb8ed2afec10bdfdd2ac75b61d75
NeerajSuriya <neerajsuriyapadmanabhan@gmail.com>
2025-08-20 10:04:26 +0000

    Added setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "medbot",
+    version = "0.0.0",
+    author = "NeerajS",
+    author_email = "neerajsuriyapadmanabhan@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
